6bc6bac6cdf9c38e3a7c2d8ccc4c48cc768bab7c
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-15 21:52:23 +0700

    initial commit

---
+++ 1.js
@@ -0,0 +1,2 @@
+//1. Buatlah fungsi dengan input array of number untuk mengurutkan angka dari terbesar ke terkecil dengan manual tanpa fungsi bawaan javascript
+//Input : [1, 2, 4, 3, 5, 3, 2, 1]			Expected Output : [5, 4, 3, 3, 2, 2, 1, 1]
