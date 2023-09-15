5170c7836c98a0a0c04e5b7ecfc4cc0ce60561f4
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-15 22:07:57 +0700

    initial commit

---
+++ 2.js
@@ -0,0 +1,4 @@
+// 2. Buatlah fungsi dengan input array of number dan number untuk menemukan jumlah maksimum subarray dengan panjang angka yang dimasukan ke fungsi
+// Input : ([100, 200, 300, 400], 2)				Expected Output : 700 		// didapatkan dari 300 + 400
+// Input : ([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)		Expected Output : 39 		// didapatkan dari 4 + 2 + 10 + 23
+// Input : ([-3, 4, 0, -2, 6, -1], 2)				Expected Output : 5 		// didapatkan dari 6 + -1
---
+++ 3.js
@@ -0,0 +1,24 @@
+// 3. Buatlah fungsi untuk mendapatkan jumlah semua bilangan genap dalam nested objek
+// Input :
+// {
+//   outer: 2,
+//   obj: {
+//     inner: 2,
+//     otherObj: {
+//       superInner: 2,
+//       notANumber: true,
+//       alsoNotANumber: "yup"
+//     }
+//   }
+// }
+// Output: 6
+
+// Input:
+// {
+//   a: 2,
+//   b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+//   c: {c: {c: 2}, cc: 'ball', ccc: 5},
+//   d: 1,
+//   e: {e: {e: 4}, ee: 'car'}
+// };
+// Output: 12
