300490c429bbe486d69e6ab3f4754389a3567d66
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-16 08:25:55 +0700

    q2

--- 2.js
+++ 2.js
@@ -2,3 +2,22 @@
 // Input : ([100, 200, 300, 400], 2)				Expected Output : 700 		// didapatkan dari 300 + 400
 // Input : ([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)		Expected Output : 39 		// didapatkan dari 4 + 2 + 10 + 23
 // Input : ([-3, 4, 0, -2, 6, -1], 2)				Expected Output : 5 		// didapatkan dari 6 + -1
+
+function getMaxSum(arr, num) {
+  let result = 0;
+  for (let i = 0; i < arr.length - (num - 1); i++) {
+    let temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+    if (result < temp) result = temp;
+  }
+  return result;
+}
+
+let result = getMaxSum([100, 200, 300, 400], 2);
+let result2 = getMaxSum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4);
+let result3 = getMaxSum([-3, 4, 0, -2, 6, -1], 2);
+console.log(result);
+console.log(result2);
+console.log(result3);
