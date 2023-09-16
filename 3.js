6d5514b9cb94a39e86db45d152c84323c94b32fb
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-16 08:26:03 +0700

    q3

--- 3.js
+++ 3.js
@@ -22,3 +22,34 @@
 //   e: {e: {e: 4}, ee: 'car'}
 // };
 // Output: 12
+
+function getSumEvenNumber(obj) {
+  let result = 0;
+  for (const key in obj) {
+    if (typeof obj[key] === "number" && obj[key] % 2 === 0) result += obj[key];
+    else if (typeof obj[key] === "object") result += getSumEvenNumber(obj[key]);
+  }
+  return result;
+}
+
+let sumEven = getSumEvenNumber({
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup",
+    },
+  },
+});
+let sumEven2 = getSumEvenNumber({
+  a: 2,
+  b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+  c: { c: { c: 2 }, cc: "ball", ccc: 5 },
+  d: 1,
+  e: { e: { e: 4 }, ee: "car" },
+});
+
+console.log(sumEven);
+console.log(sumEven2);
